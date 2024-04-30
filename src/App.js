ae34dd6beaa1f819a559e1d8399e9212bb48679c
unknown <gbengaTec>
2024-04-30 12:15:05 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../../setup/src/Navbar';
+import CartContainer from '../../setup/src/CartContainer';
+
+function App (){
+  return(
+    <main>
+      <Navbar/>
+      <CartContainer/>
+    </main>
+  )
+}
+export default App;
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    title: 'Samsung Galaxy S7',
+    price: 599.99,
+    image: 'https://itouchcommunications.com/wp-content/uploads/2023/03/Samsung-Galaxy-S7-edge.jpg',
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: 'google pixel ',
+    price: 499.99,
+    image: 'https://www.jbhifi.com.au/cdn/shop/products/662710-Product-0-I-638302499403423176.jpg?v=1707971721',
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: 'Xiaomi Redmi Note 2',
+    price: 699.99,
+    image: 'https://archiwum.allegro.pl/image//imagesNEW/big/fb637848fe3e92f6688a4d11ed109cb20e77131ecc2b2e7aa9b148a76781679c',
+    amount: 1,
+  },
+];
